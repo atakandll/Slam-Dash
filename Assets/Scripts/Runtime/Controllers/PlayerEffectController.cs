554db0e5bf9aa385f38d7136419cdf33bed60eac
atakandll <atakanccnar@outlook.com>
2024-08-13 19:16:05 +0300

    refactor(particle) : Basic pooling system implemented for dust particle.

--- Assets/Scripts/Runtime/Controllers/PlayerEffectController.cs
+++ Assets/Scripts/Runtime/Controllers/PlayerEffectController.cs
@@ -1,13 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Runtime.Controllers
 {
     public class PlayerEffectController : MonoBehaviour
     {
         [SerializeField] private GameObject dustTrailPrefab;
+        private List<GameObject> _dustParticle = new List<GameObject>();
         public void DustTrail(Vector3 worldPoint)
         {
-            Instantiate(dustTrailPrefab, worldPoint, Quaternion.identity);
+            foreach (GameObject g in _dustParticle)
+            {
+                if (!g.activeInHierarchy)
+                {
+                    g.transform.position = worldPoint;
+                    g.SetActive(true);
+                    return;
+                }
+                    
+            }
+            GameObject newDust = Instantiate(dustTrailPrefab, worldPoint, Quaternion.identity);
+            _dustParticle.Add(newDust);
       
         }
 
