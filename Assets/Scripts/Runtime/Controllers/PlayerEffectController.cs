79c92a4a4a3a71cf37a0c52b97d31945c3c9b55d
atakandll <atakanccnar@outlook.com>
2024-08-15 02:41:45 +0300

    feat(animation) : Idle, wallHit, dead, atGoal, run animation ara setup and implemented with player effect controller.

--- Assets/Scripts/Runtime/Controllers/PlayerEffectController.cs
+++ Assets/Scripts/Runtime/Controllers/PlayerEffectController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Runtime.Enums;
+using Runtime.Managers;
 using UnityEngine;
 
 namespace Runtime.Controllers
@@ -7,6 +10,28 @@ namespace Runtime.Controllers
     {
         [SerializeField] private GameObject dustTrailPrefab;
         private readonly List<GameObject> _dustParticle = new List<GameObject>();
+        private Animator _animator;
+        private GameManager _gameManager;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _gameManager = FindObjectOfType<GameManager>();
+
+        }
+
+        private void Update()
+        {
+            if(_gameManager.GetGameState() == GameState.Lost)
+            {
+                _animator.SetBool("dead",true);
+            }
+            else if(_gameManager.GetGameState() == GameState.Won)
+            {
+                _animator.SetBool("atGoal",true);
+            }
+        }
+        
         public void DustTrail(Vector3 worldPoint)
         {
             foreach (GameObject dust in _dustParticle)
@@ -26,6 +51,7 @@ namespace Runtime.Controllers
 
         public void HitWall()
         {
+            _animator.SetTrigger("hitWall");
             // Play sound
             // flashing
         }
